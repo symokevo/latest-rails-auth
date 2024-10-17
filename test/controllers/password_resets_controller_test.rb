34e17b9d302f18dd014587cb3ead620888c2322c
Simon Rabuogi <symokevo@gmail.com>
2024-10-17 11:00:30 +0300

    Adjust tests

--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -1,13 +1,57 @@
 require "test_helper"
 
 class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get password_resets_new_url
+  test "#now" do
+    get new_password_reset_path
     assert_response :success
   end
 
-  test "should get create" do
-    get password_resets_create_url
+  test "#create" do
+    assert_emails 1 do
+      post password_reset_path, params: { email: users(:simon).email }
+    end
+
+    assert_redirected_to root_path
+  end
+
+  test "#edit" do
+    get edit_password_reset_path
     assert_response :success
+  end
+
+  test "#update resets the password with a valid token and valid params" do
+    user = users(:simon)
+    token = user.generate_token_for(:password_reset)
+
+    patch password_reset_path, params: {
+      password_reset_token: token,
+      password_reset: { password: "newpassword", password_confirmation: "newpassword"}
+    }
+    assert_redirected_to dashboard_path
+    assert_signed_in
+    assert user.reload.authenticate("newpassword")
+  end
+
+  test "#update fails without a valid token" do
+    patch password_reset_path, params: {
+      password_reset_token: "invalid",
+      password_reset: { password: "newpassword", password_confirmation: "newpassword"}
+    }
+
+    assert_redirected_to new_password_reset_path
+    follow_redirect!
+    assert_select "main", /Invalid token/
+  end
+
+  test "#update fails if password and password confirmation don't match" do
+    user = users(:simon)
+    token = user.generate_token_for(:password_reset)
+
+    patch password_reset_path, params: {
+      passowrd_reset_token: token,
+      password_reset: { password: "newpassword", password_confirmation: "notmatching" }
+    }
+    assert_response :unprocessable_entity
+    assert_select "main", /Password confirmation doesn't match Password/
   end
 end
