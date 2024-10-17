f3b68453d6f2980d69bb348baf5c66d51384a554
Simon Rabuogi <symokevo@gmail.com>
2024-10-17 12:01:29 +0300

    Create password Reset

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # Login and logout
   resource :session, only: %i[new create destroy]
   resource :password_reset, only: %i[new create edit update]
+  resource :password, only: %i[edit update]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
